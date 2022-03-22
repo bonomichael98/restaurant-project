9a82efc7380e73247744764420af221858260f73
Josiah Douglass <josiahdouglass64@gmail.com>
2022-03-22 19:30:54 -0400

    fetchCocktail function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-let cocktailApiURL = 'http://www.thecocktaildb.com/api/json/v1/1/random.php';
+let fetchCocktail = function() {
+    let cocktailApiURL = 'http://www.thecocktaildb.com/api/json/v1/1/random.php';
 
-fetch(cocktailApiURL)
-    .then(function(response) {
-        return response.json();
-    })
+    fetch(cocktailApiURL) 
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data){
+                    // pick out the impostant parts of the data returned
+                    let cityLat = (data.results[0].location.lat);
+                    let cityLng = (data.results[0].location.lng);
+                    // Calls the next function
+                    weatherDataHandler(cityLat,cityLng,cityName);
+            })}});
+            
+        }
+
+fetchCocktail();
