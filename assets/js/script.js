70d5f56434ed9f8c85da845a1e899866f052f522
Josiah Douglass <josiahdouglass64@gmail.com>
2022-03-22 20:20:47 -0400

    updated api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-let cocktailApiURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
-fetch(cocktailApiURL)
-    .then(function(response) {
-        console.log(response)
-        return response.json();
-    })
+let cocktailSetup = function() {
+    for (let i = 0; i<3 ; i++) {
+        let cocktailApiURL = "www.thecocktaildb.com/api/json/v1/1/random.php";
+
+        fetch(cocktailApiURL)
+            .then(function(response) {
+                if (response.ok) {
+                    response.json().then(function(data){
+                        console.log(data);
+                        // let cocktail = data.drinks[0].strDrink;
+                        // console.log(cocktail);
+                })}});
+            }
+}
+
+cocktailSetup();
