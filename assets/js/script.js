d4ebca553caa349d151ecd4015b332a63de1277f
michael <shinlin09@gmail.com>
2022-03-23 12:49:38 -0400

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 let cocktailApiURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+let googleAutocompleteURL = "https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters&key=AIzaSyCUWf_0fYUln2C1Z1GPbLLPT3fERLMMx7M";
 
 fetch(cocktailApiURL)
     .then(function(response) {
-        console.log(response)
+        console.log(response);
         return response.json();
-    })
+    });
+
+fetch(googleAutocompleteURL)
